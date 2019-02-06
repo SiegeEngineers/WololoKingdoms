ff428a337738fa20646f9ed362fec1a47f1fcd96
Renée Kooi <renee@kooi.me>
2019-02-06 17:55:03 +0100

    Add defaults for WKSettings

--- libwololokingdoms/wksettings.h
+++ libwololokingdoms/wksettings.h
@@ -9,18 +9,63 @@ namespace fs = std::filesystem;
 
 class WKSettings
 {
-
-
 public:
-    WKSettings(bool useVoobly, bool useExe, bool useBoth, bool useMonks, bool usePw, bool useWalls, bool copyMaps, bool copyCustomMaps,
-       bool restrictedCivMods, bool useNoSnow, bool fixFlags, bool replaceTooltips, bool useGrid, fs::path installDirectory,
-       std::string language, int dlcLevel, int patch, int hotkeyChoice, fs::path hdPath, fs::path outPath, fs::path vooblyDir, fs::path upDir,
-       std::map<int, std::tuple<std::string,std::string, std::string, int, std::string>> dataModList, std::string modName)
-        : useVoobly(useVoobly), useExe(useExe), useBoth(useBoth), useMonks(useMonks), usePw(usePw), useWalls(useWalls), copyMaps(copyMaps),
-          copyCustomMaps(copyCustomMaps), restrictedCivMods(restrictedCivMods), useNoSnow(useNoSnow), fixFlags(fixFlags), replaceTooltips(replaceTooltips),
-          useGrid(useGrid), installDirectory(installDirectory), language(language), patch(patch), hotkeyChoice(hotkeyChoice), dlcLevel(dlcLevel),
-          hdPath(hdPath), outPath(outPath), vooblyDir(vooblyDir), upDir(upDir), nfzVooblyOutPath(vooblyDir/"Player.nfz"),
-          nfzUpOutPath(upDir/"Player.nfz"), dataModList(dataModList), modName(modName){}
+    typedef std::tuple<std::string,std::string, std::string, int, std::string> DataMod;
+
+    WKSettings(
+        bool useVoobly = true,
+        bool useExe = false,
+        bool useBoth = false,
+        bool useMonks = true,
+        bool usePw = true,
+        bool useWalls = false,
+        bool copyMaps = true,
+        bool copyCustomMaps = false,
+        bool restrictedCivMods = true,
+        bool useNoSnow = true,
+        bool fixFlags = false,
+        bool replaceTooltips = true,
+        bool useGrid = true,
+        fs::path installDirectory = fs::path(),
+        std::string language = "en",
+        int dlcLevel = 0,
+        int patch = -1,
+        int hotkeyChoice = 0,
+        fs::path hdPath = fs::path(),
+        fs::path outPath = fs::path(),
+        fs::path vooblyDir = fs::path(),
+        fs::path upDir = fs::path(),
+        std::map<int, DataMod> dataModList = {},
+        std::string modName = ""
+    ) : useVoobly(useVoobly),
+        useExe(useExe),
+        useBoth(useBoth),
+        useMonks(useMonks),
+        usePw(usePw),
+        useWalls(useWalls),
+        copyMaps(copyMaps),
+        copyCustomMaps(copyCustomMaps),
+        restrictedCivMods(restrictedCivMods),
+        useNoSnow(useNoSnow),
+        fixFlags(fixFlags),
+        replaceTooltips(replaceTooltips),
+        useGrid(useGrid),
+        installDirectory(installDirectory),
+        language(language),
+        patch(patch),
+        hotkeyChoice(hotkeyChoice),
+        dlcLevel(dlcLevel),
+        hdPath(hdPath),
+        outPath(outPath),
+        vooblyDir(vooblyDir),
+        upDir(upDir),
+        nfzVooblyOutPath(vooblyDir/"Player.nfz"),
+        nfzUpOutPath(upDir/"Player.nfz"),
+        dataModList(dataModList),
+        modName(modName)
+    {
+    }
+
     bool useVoobly;
     bool useExe;
     bool useBoth;
@@ -45,7 +90,7 @@ public:
     fs::path upDir;
     fs::path nfzVooblyOutPath;
     fs::path nfzUpOutPath;
-    std::map<int, std::tuple<std::string,std::string, std::string, int, std::string>> dataModList;
+    std::map<int, DataMod> dataModList;
     std::string modName;
 };
 
