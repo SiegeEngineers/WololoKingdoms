bb55e45915b828cbea1839da9e4ec892ce5ce59b
Renée Kooi <renee@kooi.me>
2019-02-09 13:45:17 +0100

    Add stub command line parser so -v and -h do what you expect

--- main.cpp
+++ main.cpp
@@ -1,24 +1,41 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QCommandLineParser>
 #include <QMessageBox>
 #ifndef _WIN32
 #include <stdlib.h>
 #endif
 
-int main(int argc, char *argv[])
-{
-  QApplication a(argc, argv);
-  MainWindow w;
-
+bool can_run_windows_programs() {
 #ifndef _WIN32
   // Make sure that Wine exists on Linux
-  int exitcode = system("wine hostname");
-  if (exitcode != 0) {
-    QMessageBox::warning(&w, "Wine not found", "Wine could not be found. You will have to pick the path to your Age of Empires 2 directory manually.", QMessageBox::Ok);
-  }
+  int exitcode = system("wine --version");
+  return exitcode == 0;
+#else
+  return true;
 #endif
+}
 
-  w.show();
+int main(int argc, char *argv[])
+{
+  QApplication application(argc, argv);
+  application.setApplicationName("WololoKingdoms");
+  application.setApplicationVersion("5.8.2");
 
-  return a.exec();
+  QCommandLineParser cli;
+  cli.setApplicationDescription("Installer for the WololoKingdoms mod, bringing Age of Empires II: HD Edition expansions to Age of Empires II: The Conquerors");
+  cli.addHelpOption();
+  cli.addVersionOption();
+
+  cli.process(application);
+
+  MainWindow window;
+
+  if (!can_run_windows_programs()) {
+    QMessageBox::warning(&window, "Wine not found", "Wine could not be found. You will have to pick the path to your Age of Empires 2 directory manually.", QMessageBox::Ok);
+  }
+
+  window.show();
+
+  return application.exec();
 }
