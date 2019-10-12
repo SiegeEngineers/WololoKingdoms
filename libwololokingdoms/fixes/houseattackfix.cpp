d7c8e94b6a6626312855943f874077308bda60f1
Travis Westura <twestura@gmail.com>
2019-10-12 17:10:44 -0400

    Remove unnecessary TODO statement

--- libwololokingdoms/fixes/houseattackfix.cpp
+++ libwololokingdoms/fixes/houseattackfix.cpp
@@ -3,7 +3,6 @@
 
 namespace wololo {
 void houseAttackFixPatch(genie::DatFile* aocDat) {
-  // TODO implement
   // Clears the task list for each house, removing the only task (which is the
   // combat task).
   const size_t unitIdsHouses[] = { 70, 463, 464, 465 };
