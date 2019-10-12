cf375b71292368ce8bd53841288fad8b21a66cc0
Travis Westura <twestura@gmail.com>
2019-10-12 17:57:43 -0400

    Remove commented out code

--- libwololokingdoms/fixes/demoshipfix.cpp
+++ libwololokingdoms/fixes/demoshipfix.cpp
@@ -9,28 +9,14 @@ void demoshipPatch(genie::DatFile* aocDat) {
    * without. However, it does full damage in the entire blast radius.
    */
 
-  // TODO remove commented out code
-  //size_t const demoRaftUnitId = 1104;
   size_t const demoShipUnitId = 527;
-  //size_t const heavyDemoShipUnitId = 528;
   
   for (size_t civIndex = 0; civIndex < aocDat->Civs.size(); civIndex++) {
-    //aocDat->Civs[civIndex].Units[demoRaftUnitId].Creatable.HeroMode = 96;
 	// Hero mode for the demo ship must be 104 so that it's attack stance defaults to Defensive
 	// when a Demolition ship is placed in the scenario editor.
     aocDat->Civs[civIndex].Units[demoShipUnitId].Creatable.HeroMode = 104;
-    //aocDat->Civs[civIndex].Units[heavyDemoShipUnitId].Creatable.HeroMode = 96;
   }
-  /*genie::TechageEffect effect = genie::TechageEffect();
-      effect.Type = 0; // set attribute
-      effect.A = demoShipUnitId;
-      effect.C = 40;
-      effect.D = 96;
-      aocDat->Techages[darkAgeTechId].Effects.push_back(effect);
-  effect.A = heavyDemoShipUnitId;
-  aocDat->Techages[demoRaftTechId].Effects.push_back(effect);
-  effect.A = demoRaftUnitId;
-  aocDat->Techages[demoRaftTechId].Effects.push_back(effect);*/
+
 }
 
 DatPatch demoShipFix = {&demoshipPatch, "Demolition ships not exploding fix"};
